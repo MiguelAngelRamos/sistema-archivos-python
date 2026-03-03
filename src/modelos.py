d09c0aff84f8e87f6bd764158d1a9ff07f9b38b3
Miguel Ramos <return.infox@gmail.com>
2026-03-02 22:29:37 -0300

    archivos

---
+++ src/modelos.py
@@ -0,0 +1,6 @@
+"""
+* Crea una clase `Estudiante` que tenga como atributos su `nombre` y una lista de `notas`.
+* La clase debe tener un método `calcular_promedio()` que retorne el promedio de sus notas.
+"""
+class Estudiante:
+    pass
